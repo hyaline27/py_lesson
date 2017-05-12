297dc74a9d885b29f01a84061c72572aa30f0c5c
hyaline27 <hyalinenov27@hontmail.com>
2017-05-12 13:52:19 +0800

    新分支dev

--- lesson.py
+++ lesson.py
@@ -1,6 +1,7 @@
-#-*- encoding=UTF-8 -*-
+# -*- encoding=UTF-8 -*-
 import requests
 from bs4 import BeautifulSoup
+
 
 def qiushibaike():
     contet = requests.get('http://www.qiushibaike.com').content
@@ -8,6 +9,7 @@ def qiushibaike():
 
     for div in soup.find_all('div', {'class': 'content'}):
         print(div.text.strip())
+
 
 def demo_string():
     stra = 'hello world'
@@ -24,25 +26,28 @@ def demo_string():
     print 7, '-'.join(['a', 'b', 'c'])
     print 8, strc.split(' ')
 
+
 def demo_operation():
-    print 1, 1+2, 5/2, 5*2,5-2
+    print 1, 1 + 2, 5 / 2, 5 * 2, 5 - 2
     print 2, True, not True
-    print 3, 1<2, 5>2, 1>4
-    print 4, 2<<3
-    print 5, 5|3, 5&3, 5^3
-    x=2
-    y=3.3
+    print 3, 1 < 2, 5 > 2, 1 > 4
+    print 4, 2 << 3
+    print 5, 5 | 3, 5 & 3, 5 ^ 3
+    x = 2
+    y = 3.3
     print x, y, type(x), type(y)
+
 
 def demo_buildinfunction():
     print 1, max(2, 1), min(5, 3)
-    print 2, len('xxx'), len([1,2,3])
-    print 3, abs(-2) #fabs,Math,fabs
+    print 2, len('xxx'), len([1, 2, 3])
+    print 3, abs(-2)  # fabs,Math,fabs
     print 4, range(1, 10, 3)
     print 5, dir(list)
     x = 2
     print 6, eval('x+3')
     print 7, chr(97), ord('a')
+
 
 def demo_controlflow():
     score = 65
@@ -56,21 +61,22 @@ def demo_controlflow():
     while score < 100:
         print score
         score += 10
-    score=65
+    score = 65
 
     # for(int i = 0; i<10; ++i)
     # continue ,break, pass
     for i in range(0, 10, 2):
         if i == 0:
-            pass #do_special
+            pass  # do_special
         if i < 5:
             continue
         print 3, i
         if i == 6:
             break
 
+
 def demo_list():
-    lista = [1,2,3]   #vector Arraylist
+    lista = [1, 2, 3]  # vector Arraylist
     print 1, lista
     listb = ['a', 1, 'c', 1.1]
     print 2, listb
@@ -91,7 +97,8 @@ def demo_list():
     print 11, listb
     listb.sort(reverse=True)
     print 12, listb
-    print 13, listb*2
+    print 13, listb * 2
+
 
 if __name__ == '__main__':
     print('hello world')
